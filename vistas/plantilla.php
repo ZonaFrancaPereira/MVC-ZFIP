51362b0ac2d11dcab7ff0de52f9fa03c10069382
Melissa <ymontoyag@zonafrancadepereira.com>
2024-06-18 17:25:31 -0500

    cabecera

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -24,119 +24,67 @@ session_start();
   <link rel="icon" href="vistas/img/plantilla/icono-negro.png">
 
    <!--=====================================
-  PLUGINS DE CSS
+  vistas/PLUGINS DE CSS
   ======================================-->
-
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
-
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="vistas/bower_components/font-awesome/css/font-awesome.min.css">
-
+  <link rel="stylesheet" href="vistas/vistas/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="vistas/bower_components/Ionicons/css/ionicons.min.css">
-
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="vistas/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="vistas/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="vistas/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="vistas/dist/css/AdminLTE.css">
-
-  <!-- AdminLTE Skins -->
-  <link rel="stylesheet" href="vistas/dist/css/skins/_all-skins.min.css">
-
-  <!-- ZOOM Image -->
-  <link rel="stylesheet" href="vistas/dist/css/zoom.css">
-
-  <!-- Google Font -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-   <!-- DataTables -->
-  <link rel="stylesheet" href="vistas/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  <link rel="stylesheet" href="vistas/bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">
-
-  <!-- iCheck for checkboxes and radio inputs -->
-  <link rel="stylesheet" href="vistas/plugins/iCheck/all.css">
-
-   <!-- Daterange picker -->
-  <link rel="stylesheet" href="vistas/bower_components/bootstrap-daterangepicker/daterangepicker.css">
-
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="vistas/bower_components/morris.js/morris.css">
-
-  <!-- Filtro Menu-->
-  <link rel="stylesheet" href="vistas/bower_components/jquery.treefilter/jquery.treefilter.css">
-
-  <!-- bootstrap datepicker -->
-  <link rel="stylesheet" href="vistas/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-
-  <!-- Select2 -->
-  <link rel="stylesheet" href="vistas/bower_components/select2/dist/css/select2.min.css">
-
- <!-- Switch Check -->
- <link href="vistas/bower_components/bootstrap-toggle/css/bootstrap-toggle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="vistas/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="vistas/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="vistas/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="vistas/plugins/summernote/summernote-bs4.min.css">
 
 
   <!--=====================================
   PLUGINS DE JAVASCRIPT
   ======================================-->
-
-  <!-- jQuery 3 -->
-  <script src="vistas/bower_components/jquery/dist/jquery.min.js"></script>
-
-  <!-- Bootstrap 3.3.7 -->
-  <script src="vistas/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-  <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
-
-  <!-- FastClick -->
-  <script src="vistas/bower_components/fastclick/lib/fastclick.js"></script>
-
-  <!-- AdminLTE App -->
-  <script src="vistas/dist/js/adminlte.min.js"></script>
-
-
-
-  <!-- DataTables -->
-  <script src="vistas/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-  <script src="vistas/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-  <script src="vistas/bower_components/datatables.net-bs/js/dataTables.responsive.min.js"></script>
-  <script src="vistas/bower_components/datatables.net-bs/js/responsive.bootstrap.min.js"></script>
-
-  <!-- SweetAlert 2 -->
-  <script src="vistas/plugins/sweetalert2/sweetalert2.all.js"></script>
-   <!-- By default SweetAlert2 doesn't support IE. To enable IE 11 support, include Promise polyfill:-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-
-  <!-- iCheck 1.0.1 -->
-  <script src="vistas/plugins/iCheck/icheck.min.js"></script>
-
-  <!-- InputMask -->
-  <script src="vistas/plugins/input-mask/jquery.inputmask.js"></script>
-  <script src="vistas/plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
-  <script src="vistas/plugins/input-mask/jquery.inputmask.extensions.js"></script>
-
-  <!-- jQuery Number -->
-  <script src="vistas/plugins/jqueryNumber/jquerynumber.min.js"></script>
-
-  <!-- daterangepicker http://www.daterangepicker.com/-->
-  <script src="vistas/bower_components/moment/min/moment.min.js"></script>
-  <script src="vistas/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-
-  <!-- Morris.js charts http://morrisjs.github.io/morris.js/-->
-  <script src="vistas/bower_components/raphael/raphael.min.js"></script>
-  <script src="vistas/bower_components/morris.js/morris.min.js"></script>
-
-  <!-- ChartJS http://www.chartjs.org/-->
-  <script src="vistas/bower_components/chart.js/Chart.js"></script>
-
-  <!-- bootstrap datepicker -->
-  <script src="vistas/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-  <script src="vistas/bower_components/bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js" charset="UTF-8" ></script>
-
-  <!-- Select2 -->
-  <script src="vistas/bower_components/select2/dist/js/select2.full.min.js"></script>
-
-
-  <!-- Swith Check -->
-  <script src="vistas/bower_components/bootstrap-toggle/js/bootstrap-toggle.min.js"></script>
+<!-- jQuery -->
+<script src="vistas/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="vistas/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="vistas/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="vistas/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="vistas/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="vistas/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="vistas/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="vistas/plugins/moment/moment.min.js"></script>
+<script src="vistas/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="vistas/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="vistas/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="vistas/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="vistas/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="vistas/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="vistas/dist/js/pages/dashboard.js"></script>
 
 
 
