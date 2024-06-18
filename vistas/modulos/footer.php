4283def31750648ac01ee680ceb0eb43f71f0b0d
Melissa <ymontoyag@zonafrancadepereira.com>
2024-06-18 17:12:41 -0500

    migracion a MVC

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+
+	<strong>Copyright &copy; 2024 <a href="https://zonafrancadepereira/" target="_blank">Zona Franca Internacional de Pereira V4</a>.</strong>
+
+	Todos los derechos reservados.
+
+</footer>
