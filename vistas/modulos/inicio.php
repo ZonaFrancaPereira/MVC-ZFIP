c58c3367a62394794e2c9bc4b3bfab5e7827f55e
Melissa <ymontoyag@zonafrancadepereira.com>
2024-06-20 13:22:20 -0500

    iconos

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -1,50 +1,20 @@
  
- <!-- Sidebar Menu -->
- <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Widgets
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-        </ul>
-      </nav>
+        <li class="nav-item En_linea 1" role="presentation">
+          <a data-toggle="tab" href="#acpm" class="nav-link">
+            <i class="nav-icon far fa-smile-wink"></i>
+            <p>
+              Novedades
+            </p>
+          </a>
+        </li>
+
+      </ul>
+
+    </nav>
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
