5077f699ffa6f008e1431981832e21753d134737
Melissa <ymontoyag@zonafrancadepereira.com>
2024-06-24 10:27:52 -0500

    migracion plataforma melissa

--- vistas/modulos/perfiles.php
+++ vistas/modulos/perfiles.php
@@ -60,8 +60,8 @@ if ($_SESSION["perfiles"] == "off") {
                             <input type="text" class="form-control input-lg" name="nuevoDescripcionPerfil" placeholder="Ejemplo: TI | Usuarios | Super Usuario" required>
                         </div>
                         <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="custom-tabs-one-home-tab" data-toggle="pill" href="#ti" role="tab" aria-controls="ti" aria-selected="true">TI</a>
+                            <li class="nav-item ">
+                                <a class="nav-link active " id="custom-tabs-one-home-tab" data-toggle="pill" href="#ti" role="tab" aria-controls="ti" aria-selected="true">TI</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill" href="#sig" role="tab" aria-controls="sig" aria-selected="false">SIG</a>
@@ -73,14 +73,20 @@ if ($_SESSION["perfiles"] == "off") {
                         </ul>
                         <div class="tab-content" id="custom-tabs-one-tabContent">
                             <div class="tab-pane fade show active" id="ti" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
-                                <div class="card-header bg-info">
+                                <!-- MODULO PARA DAR LOS PERMISOS QUE TIENE CADA USUARIO CON RELACIÓN A TI -->
+                                <div class="card-header bg-teal">
                                     Configuraciones Tecnología e Informática.
                                 </div>
-                                <!-- DIV PARA CONTENER LAS OPCIONES -->
+                                <!-- DIV PARA CONTENER LAS OPCIONES  DE TI-->
                                 <div class="pt-2 card">
-                                       <!-- Check USUARIOS -->
-                                       <div class="checkbox">
-                                        <input type="checkbox" data-toggle="toggle" name="smPerfiles" data-on="Si" data-off="No">
+                                    <!-- MODULO PARA GESTIONAR TODO LO RELACIONADO CON EL AREA DE TI, USUARIOS, PERFILES -->
+                                    <div class="card pt-2">
+                                        <h6 class="text-center">MODULO GESTIÓN TI</h6>
+                                    </div>
+
+                                    <!-- Check USUARIOS -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smUsuarios" data-on="Si" data-off="No">
                                         <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para administrar usuarios">Usuarios</label>
                                     </div>
                                     <!-- Check Perfiles -->
@@ -88,29 +94,117 @@ if ($_SESSION["perfiles"] == "off") {
                                         <input type="checkbox" data-toggle="toggle" name="smPerfiles" data-on="Si" data-off="No">
                                         <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para administrar perfiles, es importante para limitar permisos dentro de la APP">Perfiles</label>
                                     </div>
-                                     <!-- Check Mantenimientos -->
-                                     <div class="checkbox pt-2">
-                                        <input type="checkbox" data-toggle="toggle" name="smPerfiles" data-on="Si" data-off="No">
-                                        <label>Mantenimientos</label>
+                                    <br>
+                                     <!-- MODULO PARA GESTIONAR LOS MANTENIMIENTOS DE LOS EQUIPOS TECNOLÓGICOS -->
+                                     <div class="card pt-2">
+                                        <h6 class="text-center">MODULO MANTENIMIENTO DE RECURSOS TECNOLÓGICOS</h6>
                                     </div>
-                                     <!-- Check Inventario TI -->
-                                     <div class="checkbox pt-2">
-                                        <input type="checkbox" data-toggle="toggle" name="smPerfiles" data-on="Si" data-off="No">
-                                        <label>Inventario TI</label>
+                                    <!-- Check Mantenimientos -->
+                                    <div class="checkbox pt-2">
+                                        <input type="checkbox" data-toggle="toggle" name="smMantenimientos" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para administrar los mantenimientos de equipos y dispositivos de ZFIP">Mantenimientos</label>
                                     </div>
-
+                                    <!-- Check Inventario TI -->
+                                    <div class="checkbox pt-2">
+                                        <input type="checkbox" data-toggle="toggle" name="smInventario" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para administrar el inventario de recursos tecnológicos, incluidas licencias, dispositivos móviles y equipos de computo">Inventario TI (Asignación de equipos y licencias)</label>
+                                    </div>
+                                    <br>
+                                    <!-- MODULO PARA GESTIONAR LOS SOPORTES TÉCNICOS -->
+                                    <div class="card pt-2">
+                                        <h6 class="text-center">MODULO SOPORTE TÉCNICO</h6>
+                                    </div>
+                                    <!-- Check Solicitud de soporte TI -->
+                                    <div class="checkbox pt-2">
+                                        <input type="checkbox" data-toggle="toggle" name="smSolicitudSoporte" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para enviar solicitudes de soporte técnico al area de TI">Solicitud de Soporte TI</label>
+                                    </div>
+                                     <!-- Check CONSULTAR SOLICITUDES POR CADA USUARIO -->
+                                     <div class="checkbox pt-2">
+                                        <input type="checkbox" data-toggle="toggle" name="smConsultarSoporte" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para consultar solicitudes de soporte técnico realizadas">Consultar Solicitudes de Soporte Técnico </label>
+                                    </div>
+                                    <!-- Check Administrador de soportes TI -->
+                                    <div class="checkbox pt-2">
+                                        <input type="checkbox" data-toggle="toggle" name="smAdminSoporte" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para responder solicitudes de soporte técnico a los usuarios">Administrador de Soportes TI</label>
+                                    </div>
+                                    <br>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="sig" role="tabpanel" aria-labelledby="custom-tabs-one-profile-tab">
-                                Configuraciones Sistema Integrado de Gestión.
+                                <!-- MODULO PARA DAR LOS PERMISOS QUE TIENE CADA USUARIO CON RELACIÓN A SISTEMA INTEGRADO DE GESTIÓN SIG -->
+                                <div class="card-header bg-success">
+                                    Configuraciones Sistema Integrado de Gestión.
+                                </div>
+                                <!-- DIV PARA CONTENER LAS OPCIONES  DE SIG-->
+                                <div class="pt-2 card">
+                                    <!-- Check RECHAZAR-ACEPTAR-CERRAR ACPM SOLO CORRESPONDE AL SIG -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smUsuarios" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para rechazar, aceptar y cerrar ACPM de los lideres de proceso.">Administrar ACPM</label>
+                                    </div>
+                                    <!-- Check RADICAR ACPM -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smCrearAcpm" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para radicar una acpm, solo lideres de proceso y personal autorizado">Crear ACPM</label>
+                                    </div>
+                                    <!-- Check ASIGNAR ACTIVIDADES -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smAsignarActividades" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para asignar actividades a los colaboradores">Asignar Actividades</label>
+                                    </div>
+                                    <!-- Check RESPONDER ACTIVIDADES -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smResponderActividades" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para responder actividades que tienen asignadas">Responder Actividades</label>
+                                    </div>
+                                    <!-- Check CONSULTAR ACPM -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smResponderActividades" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para consultar las ACPM que se tienen en el proceso, solo para lideres de proceso ó personal autorizado.">Consultar ACPM</label>
+                                    </div>
+                                </div>
                             </div>
                             <div class="tab-pane fade" id="ct" role="tabpanel" aria-labelledby="custom-tabs-one-messages-tab">
-                                Configuraciones Contabilidad y Finanzas.
+                                <!-- MODULO PARA DAR LOS PERMISOS QUE TIENE CADA USUARIO CON RELACIÓN A CONTABILIDAD -->
+                                <div class="card-header bg-warning">
+                                    Configuraciones Contabilidad y Finanzas.
+                                </div>
+
+                                <div class="pt-2 card">
+                                    <!-- DIV PARA CONTENER LAS OPCIONES  DE LAS ORDENES DE COMPRA-->
+                                    <h6 class="text-center">MODULO ORDENES DE COMPRA</h6>
+                                    <hr>
+                                    <!-- Check CREAR ORDENES DE COMPRA -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smUsuarios" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para rechazar, aceptar y cerrar ACPM de los lideres de proceso.">Administrar ACPM</label>
+                                    </div>
+                                    <!-- Check ADMINISTRAR PROVEEDORES -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smCrearAcpm" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para radicar una acpm, solo lideres de proceso y personal autorizado">Crear ACPM</label>
+                                    </div>
+                                    <!-- Check CONSULTAR ORDENES DE COMPRA -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smAsignarActividades" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para asignar actividades a los colaboradores">Asignar Actividades</label>
+                                    </div>
+                                    <!-- Check RESPONDER ACTIVIDADES -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smResponderActividades" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para responder actividades que tienen asignadas">Responder Actividades</label>
+                                    </div>
+                                    <!-- Check CONSULTAR ACPM -->
+                                    <div class="checkbox">
+                                        <input type="checkbox" data-toggle="toggle" name="smResponderActividades" data-on="Si" data-off="No">
+                                        <label for="exampleInputEmail1" data-toggle="tooltip" data-placement="top" title="Permisos para consultar las ACPM que se tienen en el proceso, solo para lideres de proceso ó personal autorizado.">Consultar ACPM</label>
+                                    </div>
+                                </div>
+
                             </div>
-
                         </div>
-
-
                     </div>
                     <div class="modal-footer justify-content-between">
                         <button type="button" class="btn bg-danger" data-dismiss="modal">Cancelar</button>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -46,31 +46,8 @@ session_start();
   <link rel="stylesheet" href="vistas/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="vistas/plugins/summernote/summernote-bs4.min.css">
-<<<<<<< HEAD
 <!-- Select2 -->
 <link rel="stylesheet" href="vistas/plugins/select2/css/select2.min.css">
-=======
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="vistas/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="vistas/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="vistas/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="vistas/plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="vistas/dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="vistas/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="vistas/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="vistas/plugins/summernote/summernote-bs4.min.css">
->>>>>>> 4566a30ca7bc70438da3b96b74f53da5eb05d9c1
 
  <!-- Switch Check -->
  <link href="vistas/bower_components/bootstrap-toggle/css/bootstrap-toggle.min.css" rel="stylesheet">
@@ -113,7 +90,6 @@ session_start();
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="vistas/dist/js/pages/dashboard.js"></script>
 
-<<<<<<< HEAD
   <!-- Select2 -->
   <script src="vistas/plugins/select2/js/select2.full.min.js"></script>
 
@@ -128,43 +104,6 @@ session_start();
     $('[data-toggle="tooltip"]').tooltip();
   });
 </script>
-=======
-<script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="vistas/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="vistas/plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="vistas/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="vistas/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="vistas/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="vistas/plugins/moment/moment.min.js"></script>
-<script src="vistas/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="vistas/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="vistas/plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="vistas/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<script src="vistas/dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="vistas/dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="vistas/dist/js/pages/dashboard.js"></script>
-<!-- jQuery -->
-<script src="vistas/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="vistas/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-
-
->>>>>>> 4566a30ca7bc70438da3b96b74f53da5eb05d9c1
 </head>
 
 <!--=====================================
